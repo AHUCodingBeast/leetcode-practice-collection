bb70c1bc8b358429089b9f037163d278690350b8
AHUCodingBeast <37865592+AHUCodingBeast@users.noreply.github.com>
2025-03-07 17:07:03 +0800

    优化代码结构

--- src/main/java/com/ahu/coding/beast/training/phase_dp/Question43.java
+++ src/main/java/com/ahu/coding/beast/training/phase_dp/Question43.java
@@ -51,20 +51,28 @@ public class Question43 {
 
         int[][] dp = new int[matrix.length][matrix[0].length];
 
+        // key:当前节点  value: 父节点
         Map<Node, Node> link = new LinkedHashMap<>();
         link.put(new Node(0, 0, matrix[0][0]), null);
 
         for (int i = 0; i < dp.length; i++) {
             for (int j = 0; j < dp[0].length; j++) {
                 if (i == 0 && j == 0) {
+
                     dp[i][j] = matrix[0][0];
+
                 } else if (i == 0) {
+
                     dp[i][j] = dp[i][j - 1] + matrix[i][j];
                     link.put(new Node(i, j, matrix[i][j]), new Node(i, j - 1, matrix[i][j - 1]));
+
                 } else if (j == 0) {
+
                     dp[i][j] = dp[i - 1][j] + matrix[i][j];
                     link.put(new Node(i, j, matrix[i][j]), new Node(i - 1, j, matrix[i - 1][j]));
+
                 } else {
+
                     // dp[i][j] = max{dp[i-1][j] + num[i][j] ,  dp[i][j-1] + num[i][j]}
                     if (dp[i - 1][j] > dp[i][j - 1]) {
                         dp[i][j] = dp[i - 1][j] + matrix[i][j];
@@ -73,6 +81,7 @@ public class Question43 {
                         dp[i][j] = dp[i][j - 1] + matrix[i][j];
                         link.put(new Node(i, j, matrix[i][j]), new Node(i, j - 1, matrix[i][j - 1]));
                     }
+                    
                 }
             }
         }
